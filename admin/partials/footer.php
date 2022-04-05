c4c6c90da296931ac70639cb6dc62d72ed6a2c09
Khan Mohammad Anas Maksood Alam <64961475+anas-khan01@users.noreply.github.com>
2022-04-05 10:06:12 +0530

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- footer section starts -->
+<div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2021 All rights reserved, Foooooodies. Developed By - <a href="a">GROUP 18</a></p>
+            </div>
+        </div>    
+        <!-- footer section ends-->
+
+    </body>
+</html>

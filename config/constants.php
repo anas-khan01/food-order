f338c599fb47c09e8728949718ebb27a6a82afd7
Khan Mohammad Anas Maksood Alam <64961475+anas-khan01@users.noreply.github.com>
2022-04-25 13:56:28 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -2,16 +2,16 @@
     // start session
     session_start();
     // create constants to store non repeating values
-    define('LOCALHOST','localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'food-order');
+    define('LOCALHOST','database-1.cvblelbxju0b.us-east-1.rds.amazonaws.com');
+    define('DB_USERNAME', 'admin');
+    define('DB_PASSWORD', 'temp1234');
+    define('DB_NAME', 'food');
     define('SITEURL', 'http://localhost/food-order/');
     
 
 
-    $conn=mysqli_connect('localhost','root','') or die(mysqli_error()); //connecting database
-    $db_select=mysqli_select_db($conn,'food-order') or die(mysqli_error()); //selecting database
+    $conn=mysqli_connect('database-1.cvblelbxju0b.us-east-1.rds.amazonaws.com','admin','temp1234') or die(mysqli_error()); //connecting database
+    $db_select=mysqli_select_db($conn,'food') or die(mysqli_error()); //selecting database
             
 
 
